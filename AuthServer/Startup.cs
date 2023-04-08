5c89b6c7448c9d1bf9abedb2d1c7adf2f10b2257
Sangeeth Nandakumar <twileloop@outlook.com>
2023-04-08 17:14:50 +0530

    feat - Added forwarded symetry

--- AuthServer/Startup.cs
+++ AuthServer/Startup.cs
@@ -9,36 +9,30 @@ using Microsoft.Extensions.Hosting;
 using System;
 using Westwind.AspNetCore.LiveReload;
 
-namespace AuthServer
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace AuthServer {
+    public class Startup {
+        public Startup(IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
-        public void ConfigureServices(IServiceCollection services)
-        {
+        public void ConfigureServices(IServiceCollection services) {
             var config = new OAuthConfig();
             Configuration.Bind("OAuth", config);
 
             services.AddSingleton<IUserService, UserService>();
             services.Configure<DbConfig>(Configuration.GetSection("ConnectionStrings"));
-            
-            services.AddIdentityServer(options =>
-            {
+
+            services.AddIdentityServer(options => {
                 options.Authentication.CookieLifetime = TimeSpan.FromSeconds(config.IdentityServerCookieLifetime);
-                options.IssuerUri = "https://auth.twileloop.com";
             })
-                .AddDeveloperSigningCredential()
-                .AddCorsPolicyService<MyCORSPolicy>()
-                .AddResourceStore<MyResourceStore>()
-                .AddClientStore<MyClientStore>()
-                .AddProfileService<ProfileService>()
-                .AddDeveloperSigningCredential();
+            .AddDeveloperSigningCredential()
+            .AddCorsPolicyService<MyCORSPolicy>()
+            .AddResourceStore<MyResourceStore>()
+            .AddClientStore<MyClientStore>()
+            .AddProfileService<ProfileService>()
+            .AddDeveloperSigningCredential();
 
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
             services.AddLiveReload();
@@ -56,22 +50,16 @@ namespace AuthServer
             app.UseRouting();
 
             //reverse Proxy Settings
-            var forwardOptions = new ForwardedHeadersOptions {
+            app.UseForwardedHeaders(new ForwardedHeadersOptions {
                 ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto,
                 RequireHeaderSymmetry = false
-            };
-            forwardOptions.KnownNetworks.Clear();
-            forwardOptions.KnownProxies.Clear();
-            app.UseForwardedHeaders(forwardOptions);
-
-
+            });
 
             app.UseIdentityServer();
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
+            app.UseEndpoints(endpoints => {
                 endpoints.MapDefaultControllerRoute();
             });
         }
