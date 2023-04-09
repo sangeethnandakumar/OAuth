d843a0322feedd41af1b289d0b243df625cc9b5c
Sangeeth Nandakumar <twileloop@outlook.com>
2023-04-10 00:27:52 +0530

    feat - API CORS made open

--- ApiA/Startup.cs
+++ ApiA/Startup.cs
@@ -1,51 +1,35 @@
 using IdentityModel;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace ApiA
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace ApiA {
+    public class Startup {
+        public Startup(IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
+        public void ConfigureServices(IServiceCollection services) {
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
+            services.AddSwaggerGen(c => {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ApiA", Version = "v1" });
             });
             services.AddAuthentication("Bearer")
-           .AddJwtBearer("Bearer", options =>
-           {
+           .AddJwtBearer("Bearer", options => {
                options.Authority = "https://localhost:5005";
-               options.TokenValidationParameters = new TokenValidationParameters
-               {
+               options.TokenValidationParameters = new TokenValidationParameters {
                    ValidateAudience = false
                };
            });
-            services.AddAuthorization(options =>
-            {
-                options.AddPolicy("ApiScope", policy =>
-                {
+            services.AddAuthorization(options => {
+                options.AddPolicy("ApiScope", policy => {
                     policy.RequireAuthenticatedUser();
                     policy.RequireClaim(JwtClaimTypes.Scope, "Api1");
                 });
@@ -54,10 +38,8 @@ namespace ApiA
                 //options.AddPolicy("AccountRead", policy => policy.RequireClaim(JwtClaimTypes.Scope, "account.read"));
                 //[Authorize(Policy = "Account")]
             });
-            services.AddCors(options =>
-            {
-                options.AddPolicy("default", policy =>
-                {
+            services.AddCors(options => {
+                options.AddPolicy("default", policy => {
                     policy.WithOrigins("https://localhost:44334")
                         .AllowAnyHeader()
                         .AllowAnyMethod();
@@ -66,10 +48,8 @@ namespace ApiA
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiA v1"));
@@ -78,13 +58,17 @@ namespace ApiA
             app.UseHttpsRedirection();
 
             app.UseRouting();
-            app.UseCors("default");
+
+            //app.UseCors("default");
+            app.UseCors(builder => builder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
 
             app.UseAuthentication();
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
+            app.UseEndpoints(endpoints => {
                 endpoints.MapControllers().RequireAuthorization("ApiScope");
             });
         }
